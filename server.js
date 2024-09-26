2f307fefbb90da17b4f28bc1055b05fa21e25ea4
yangyanming1126 <164983809+yangyanming1126@users.noreply.github.com>
2024-09-26 20:33:10 +1000

    pt1

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const donationsRouter = require('./api'); // 导入API路由
 
 app.use('/api', donationsRouter); // 使用API路由
 
-const PORT = process.env.PORT || 0110;
+const PORT = process.env.PORT || 1126;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
